4c5e062e47dd126679757cfe8dd67492981471d4
scorpio <muhammedlameesmv@gmail.com>
2023-08-21 10:36:48 +0530

    up

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
+
+echo "Installing requirements..."
 pip install -r requirements.txt
+echo "makemigrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+echo "collect static..."
 python manage.py collectstatic
-python manage.py migrate
-python manage.py runserver
+
+
+# #!/bin/bash
+
+# echo "Installing requirements..."
+# python3.9 -m pip install -r requirements.txt
+# echo "makemigrations..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
+# echo "collect static..."
+# python3.9 manage.py collectstatic
