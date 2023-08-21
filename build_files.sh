34c51163bb813a1f5adfa3ba3ea3bf0307f679c0
_lami_iz <86532578+Sb-4Ma6DE5H@users.noreply.github.com>
2023-08-21 11:29:51 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -11,13 +11,13 @@
 
 #!/bin/bash
 
-echo "Installing requirements..."
-python3.9 -m pip install -r requirements.txt
-echo "makemigrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-echo "collect static..."
-python3.9 manage.py collectstatic
-echo "Creating superuser..."
+# echo "Installing requirements..."
+# python3.9 -m pip install -r requirements.txt
+# echo "makemigrations..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
+# echo "collect static..."
+# python3.9 manage.py collectstatic
+# echo "Creating superuser..."
 # DJANGO_SUPERUSER_PASSWORD=a python3.9 manage.py createsuperuser --noinput --username=a --email=magegi4827@chodyi.com
-python3.9 manage.py createsuperuser --noinput --username=a --email=ymagegi4827@chodyi.com --password=a
+python3.9 manage.py createsuperuser --noinput --username=a --email=ymagegi4827@chodyi.com
