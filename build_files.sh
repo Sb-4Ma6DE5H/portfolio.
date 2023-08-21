c575416da86f0740dd9f04422702505900993a05
_lami_iz <86532578+Sb-4Ma6DE5H@users.noreply.github.com>
2023-08-21 10:39:32 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-
-echo "Installing requirements..."
-pip install -r requirements.txt
-echo "makemigrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-echo "collect static..."
-python manage.py collectstatic
-
-
 # #!/bin/bash
 
 # echo "Installing requirements..."
-# python3.9 -m pip install -r requirements.txt
+# pip install -r requirements.txt
 # echo "makemigrations..."
-# python3.9 manage.py makemigrations --noinput
-# python3.9 manage.py migrate --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 # echo "collect static..."
-# python3.9 manage.py collectstatic
+# python manage.py collectstatic
+
+
+#!/bin/bash
+
+echo "Installing requirements..."
+python3.9 -m pip install -r requirements.txt
+echo "makemigrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "collect static..."
+python3.9 manage.py collectstatic
